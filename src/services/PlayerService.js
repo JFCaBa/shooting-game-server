ad510c9de280aaa8039a009622c461cc55090b4c
Jose Catala <jfca68@gmail.com>
2024-11-25 18:42:27 +0100

    Fixed an issue with the disconnection

--- src/services/PlayerService.js
+++ src/services/PlayerService.js
@@ -2,25 +2,36 @@ const Player = require('../models/Player');
 const logger = require('../utils/logger');
 
 class PlayerService {
-  async updatePlayerStatus(playerId) {
+  async updatePlayerStatus(playerId, playerData) {
     try {
-      await Player.findOneAndUpdate(
+      return await Player.findOneAndUpdate(
         { playerId },
-        { lastActive: new Date() },
-        { upsert: true }
+        { 
+          $set: {
+            lastActive: new Date(),
+            location: playerData?.location,
+            heading: playerData?.heading
+          }
+        },
+        { upsert: true, new: true }
       );
     } catch (error) {
       logger.error('Error updating player status:', error);
     }
   }
 
-  async getPlayerStats(playerId) {
-    try {
-      return await Player.findOne({ playerId });
-    } catch (error) {
-      logger.error('Error getting player stats:', error);
-      return null;
-    }
+  async getActivePlayers() {
+    const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
+    return await Player.find({
+      lastActive: { $gte: fiveMinutesAgo }
+    });
+  }
+
+  async removeInactivePlayers() {
+    const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
+    return await Player.deleteMany({
+      lastActive: { $lt: fiveMinutesAgo }
+    });
   }
 }
 
