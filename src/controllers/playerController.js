53519e8f4e4e7292ff0b454f6ff20decb1d81f10
Jose Catala <jfca68@gmail.com>
2024-11-30 19:06:17 +0300

    Added ad reward tokens

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,4 +1,5 @@
 const PlayerService = require('../services/PlayerService');
+const logger = require('../utils/logger');
 const playerService = new PlayerService();  // Instantiate the class
 
 exports.getTokenBalance = async (req, res) => {
@@ -22,5 +23,18 @@ exports.transferTokens = async (req, res) => {
     res.json({ message: 'Tokens transferred successfully' });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+exports.adReward = async (req, res) => {
+  logger.debug("adReward endpoint hit with body:", req.body); 
+  try {
+    const { playerId } = req.body;
+    await playerService.adReward(playerId);
+    logger.debug(`Reward tokens added succesfully to: ${playerId}`)
+    res.json({ message: 'Reward tokens added successfully', ammount: 10 });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    logger.error(error)
   }
 };
--- src/routes/achievementRoutes.js
+++ src/routes/achievementRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AchievementController = require('../controllers/achievementController');
+const AchievementController = require('../controllers/AchievementController');
 
 router.get('/players/:playerId/achievements', AchievementController.getAchievements);
 router.post('/achievements/track', AchievementController.trackProgress);
